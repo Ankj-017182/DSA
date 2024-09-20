4afecad8103ea0d3e4d87360386189e54a675015
Ankj-017/FLASH <jhaankit017@gmail.com>
2024-09-20 18:07:46 +0530

    New change

--- src/main/java/dsa/practice/MaximumSumSubarray.java
+++ src/main/java/dsa/practice/MaximumSumSubarray.java
@@ -28,6 +28,34 @@ package dsa.practice;
 //}
 
 
+//If arraylist is used instead of array and we have to take user input
+
+//class Solution{  //solved on gfg and copy pasted here
+//    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+//        // code here
+//        long max = Integer.MIN_VALUE;
+//        int i=0;
+//        int j=0;
+//        long sum =0;
+//
+//        while(j<N){
+//            sum+=Arr.get(j);
+//            if(j-i+1<K)
+//                j++;
+//
+//            else if(j-i+1==K){
+//                max = Math.max(max,sum);
+//                sum -= Arr.get(i);
+//                i++;
+//                j++;
+//            }
+//        }
+//        return max;
+//
+//    }
+//}
+
+
 //Minimum sum subarray//
 
 public class MaximumSumSubarray {
